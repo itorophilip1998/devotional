deafb271f7be11524881ff679dd2bce6b5645590
itphilip1998 <itkeyzzpianny@gmail.com>
2022-07-15 07:50:38 +0100

    dev

--- src/pages/Manual/components/DiscussionQuestion.jsx
+++ src/pages/Manual/components/DiscussionQuestion.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
 
-function DiscussionQuestion() {
+function DiscussionQuestion({ item }) {
   return (
-    <div>DiscussionQuestion</div>
-  )
+    <div className="introText">
+      {/* start text */}
+      <div className="container p-2 ">
+        <span className="text d-block text-primary my-3">
+          Discussion Question:
+        </span>
+
+        <span className="text-content introduction text-dark d-block  text-justify">
+          <div  clasName="p1">
+            {console.log(item.discussion_question)}
+            {item &&
+              item?.discussion_question?.map((currentItem, index) => {
+                return (
+                  <React.Fragment key={index}>
+                    {/* questions */}
+                    <div className="question text-primary">
+                      {currentItem?.question}
+                    </div>
+
+                    {/* answers */}
+                    <ul>
+                      {currentItem &&
+                        currentItem?.answer?.map((newItem, newIndex) => {
+                          return (
+                            <li key={newIndex}>
+                              <div className="anwser text-dark">{newItem}</div>
+                            </li>
+                          );
+                        })}
+                    </ul>
+                  </React.Fragment>
+                );
+              })}
+          </div>
+        </span>
+      </div>
+      {/* end text */}
+    </div>
+  );
 }
 
-export default DiscussionQuestion
+export default DiscussionQuestion;
