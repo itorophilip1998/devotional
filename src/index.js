f0c1b5439ee883f92ac2469d9daf1daa52b06ecf
itphilip1998 <itkeyzzpianny@gmail.com>
2022-07-16 13:14:47 +0100

    no scale set final

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App'; 
+import * as serviceWorker from "./service-worker"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,3 +10,4 @@ root.render(
     <App />
   </React.StrictMode>
 ); 
+serviceWorker.unregister();
