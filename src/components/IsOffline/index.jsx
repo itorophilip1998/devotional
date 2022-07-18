a0015e8767d608b0a4970e54401eb80163cf6f6b
itphilip1998 <itkeyzzpianny@gmail.com>
2022-07-18 23:59:45 +0100

    push tested

--- src/components/IsOffline/index.jsx
+++ src/components/IsOffline/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-function IsOffline() {
-  return (
-    <div>
-      <div class="alert alert-warning" role="alert">
-        <strong>warning</strong>
+function IsOffline() { 
+  if (!navigator.onLine) 
+    return (
+      <div>
+        <div class="alert alert-warning" role="alert">
+          <strong>warning</strong>
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default IsOffline;
