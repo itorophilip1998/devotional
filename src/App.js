cf9b02fa0a70a797e9a2321428903a36eb161c82
itphilip1998 <itkeyzzpianny@gmail.com>
2022-07-18 17:37:00 +0100

    using yarn

--- src/App.js
+++ src/App.js
@@ -2,35 +2,37 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/Auth/Signin/Signin";
 import SignUp from "./pages/Auth/Signup/Sigup";
 import ForgotPassword from "./pages/Auth/ForgotPassword/ForgotPassword";
-import ResetPassword from "./pages/Auth/ResetPassword/ResetPassword"; 
+import ResetPassword from "./pages/Auth/ResetPassword/ResetPassword";
 import ManualList from "./pages/Manual/ManualList";
 import Devotional from "./pages/Devotional/Devotional";
 import Manual from "./pages/Manual/Manual";
 import Footer from "./layouts/Footer";
 import DevotionalList from "./pages/Devotional/DevotionalList";
 import Header from "./layouts/Headers/Header";
-import Profile from "./pages/Profile"; 
+import Profile from "./pages/Profile";
 import Tip from "./pages/Tip/Tip";
 import Subscribe from "./pages/Subscribe/Subscribe";
 import ProtectedRoutes from "./Middleware/ProtectedRoutes";
+import IsOffline from "./components/IsOffline";
 
 
 function App() {
-  
+
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
+      <IsOffline />
       <Routes>
         {/* auth routes */}
-        <Route path="/" element={<ProtectedRoutes />} > 
-        <Route path="/" element={<DevotionalList />} />
-        <Route path="/devotional" element={<DevotionalList />} />
-        <Route path="/manual" element={<ManualList />} />  
-        <Route path="/devotional/:topic" element={<Devotional />} />
-        <Route path="/manual/:topic" element={<Manual />} /> 
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/tips" element={<Tip />} />
-        <Route path="/subscribe" element={<Subscribe />} />
+        <Route path="/" element={<ProtectedRoutes />} >
+          <Route path="/" element={<DevotionalList />} />
+          <Route path="/devotional" element={<DevotionalList />} />
+          <Route path="/manual" element={<ManualList />} />
+          <Route path="/devotional/:topic" element={<Devotional />} />
+          <Route path="/manual/:topic" element={<Manual />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/tips" element={<Tip />} />
+          <Route path="/subscribe" element={<Subscribe />} />
         </Route>
 
         {/* guest routes */}
@@ -38,7 +40,7 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
-     
+
       </Routes>
       <Footer />
     </BrowserRouter >
---
+++ src/components/IsOffline/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function IsOffline() {
+  return (
+    <div>
+      <div class="alert alert-warning" role="alert">
+        <strong>warning</strong>
+      </div>
+    </div>
+  );
+}
+
+export default IsOffline;
