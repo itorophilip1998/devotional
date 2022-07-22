2809087ea71f30bd0fbb212ad8b45f391ab4bacc
itphilip1998 <itkeyzzpianny@gmail.com>
2022-07-22 01:02:29 +0100

    setup voice fnal setup

--- src/utils/Settings/index.js
+++ src/utils/Settings/index.js
@@ -1,17 +1,25 @@
-export const Settings = ({ rate, volume, voice ,text}) => {
-   try {
-       var msg = new SpeechSynthesisUtterance()
-       var voices = window.speechSynthesis.getVoices()
-       console.log(voice)
-       msg.voice = voices[voice] //[6]
-       msg.volume = volume // From 0 to 1
-       msg.rate = rate // From 0.1 to 10
-       msg.pitch = 0 // From 0 to 2
-       msg.lang = 'en-US'
-       msg.text = text
-       speechSynthesis.speak(msg)
-   } catch (error) {
-    console.log(error)
-   }
+export const Settings = ({ rate, volume, voice, text }) => {
+    try {
+        const speaking = window.speechSynthesis.speaking
+
+        var msg = new SpeechSynthesisUtterance()
+        var voices = window.speechSynthesis.getVoices()
+        msg.voice = voices[voice] //[6]
+        msg.volume = volume // From 0 to 1
+        msg.rate = rate // From 0.1 to 10
+        msg.pitch = 0 // From 0 to 2
+        msg.lang = 'en-US'
+        msg.text = text
+        if (!speaking) {
+            speechSynthesis.speak(msg)
+        }
+        // else {
+        //     window.speechSynthesis.paused()
+        // }
+
+        console.log(window.speechSynthesis)
+    } catch (error) {
+        console.log(error)
+    }
 }
- 
+
